cb6ad9036ab2e440e6e4832339ea39b652e10647
Mathijs Bernson <mathijsb@q42.nl>
2023-12-08 11:20:31 +0100

    Initial commit

---
+++ Template/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Template
+//
+//  Created by Mathijs Bernson on 08/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Template/TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateApp.swift
+//  Template
+//
+//  Created by Mathijs Bernson on 08/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
