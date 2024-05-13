77b901b7c0061011538d6a0d606f42ee97e9c0e0
Mathijs Bernson <mathijsb@q42.nl>
2024-05-13 12:11:58 +0200

    Add comments to UI test

--- TemplateAppUITests/ExampleUITests.swift
+++ TemplateAppUITests/ExampleUITests.swift
@@ -15,11 +15,16 @@ final class ExampleUITests: XCTestCase {
         continueAfterFailure = false
 
         let app = XCUIApplication()
+        // Custom launch arguments can be set here
+        // app.launchArguments = ["testing"]
         app.launch()
+        // A scenario provides the starting point of your tests ('GIVEN <screen>'). In each test case,
+        // you chain behaviors ('WHEN I do <action>') and assertions ('THEN I expect <result>') using the scenario.
         scenario = Scenario(given: app)
     }
 
     func testExample() {
+        // Example of a behavior-driven test
         scenario
             .then { homeView in
                 XCTAssertFalse(homeView.userEmailLabel.exists)
