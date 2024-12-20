326bf734d70550dc89425a0fdff064aef14ce034
Mathijs Bernson <mathijsb@q42.nl>
2024-12-20 15:26:55 +0100

    Trust Xcode package plugins and macros on Xcode Cloud

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,3 @@
+# Trust Xcode package plugins and macros
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
