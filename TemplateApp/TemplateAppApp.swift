1944839ae76f37b09bb49c36fcdefe2b5f5d42f6
Edwin Veger <edwin.veger@q42.nl>
2026-06-01 13:12:05 +0200

    Replace Factory with `swift-dependencies`

--- TemplateApp/TemplateAppApp.swift
+++ TemplateApp/TemplateAppApp.swift
@@ -5,15 +5,27 @@
 //  Copyright © 2024 Q42. All rights reserved.
 //
 
+import Dependencies
 import SwiftUI
 
 @main
 struct TemplateAppApp: App {
     @UIApplicationDelegateAdaptor var appDelegate: TemplateAppAppDelegate
 
+    init() {
+        bootstrap()
+    }
+
     var body: some Scene {
         WindowGroup {
             RootView()
         }
+    }
+
+    func bootstrap() {
+        // don't bootstrap the app if we're testing
+        guard context == .live else { return }
+
+        // TODO: Set up dependencies
     }
 }
